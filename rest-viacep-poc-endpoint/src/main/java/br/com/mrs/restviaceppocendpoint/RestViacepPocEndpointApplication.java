efaf35b526a061563e3f58499792d50c88a73e3c
Marlus Rudi Saade <marlus.saade@telefonica.com>
2024-03-26 09:34:29 -0300

    fix: files with Hexagonal-vexa

---
+++ rest-viacep-poc-endpoint/src/main/java/br/com/mrs/restviaceppocendpoint/RestViacepPocEndpointApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrs.restviaceppocendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestViacepPocEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestViacepPocEndpointApplication.class, args);
+	}
+
+}
---
+++ rest-viacep-poc-endpoint/src/test/java/br/com/mrs/restviaceppocendpoint/RestViacepPocEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrs.restviaceppocendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestViacepPocEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
